393f61877ea6ffb21edcfeb18332ac4315e2a6cd
shouvickpaul <shouvickp@gmail.com>
2023-09-02 15:45:00 +0530

    Saving user info in user table

---
+++ src/main/java/com/example/keycloaktest/model/User.java
@@ -0,0 +1,101 @@
+package com.example.keycloaktest.model;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "_user")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class User {
+    @Id
+    @Column(name = "identity_id", nullable = false)
+    private String identityId;
+
+    @Column(name = "login_id", nullable = false)
+    private String loginId;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email_id", nullable = false)
+    private String emailId;
+
+    @Column(name = "user_type", nullable = false)
+    private String userType;
+
+    @Column(name = "enabled", nullable = false)
+    private boolean enabled;
+
+    public String getIdentityId() {
+        return identityId;
+    }
+
+    public void setIdentityId(String identityId) {
+        this.identityId = identityId;
+    }
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public String toString() {
+        return "User [identityId=" + identityId + ", loginId=" + loginId + ", firstName=" + firstName + ", lastName="
+                + lastName + ", emailId=" + emailId + ", userType=" + userType + ", enabled=" + enabled + "]";
+    }
+
+}
